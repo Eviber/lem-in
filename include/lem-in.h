c1522d3ca88d5096f889a864404c8ad1bda10f2b
Youva GAUDE <ygaude@student.42.fr>
2017-12-13 21:04:09 +0100

    import .gitignore and lem-in.h from master

--- include/lem-in.h
+++ include/lem-in.h
@@ -6,7 +6,7 @@
 /*   By: ygaude <ygaude@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/03 15:28:55 by ygaude            #+#    #+#             */
-/*   Updated: 2017/12/13 18:28:05 by ygaude           ###   ########.fr       */
+/*   Updated: 2017/12/13 19:55:24 by ygaude           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +24,25 @@ typedef struct		s_room
 	struct s_room	**links;
 	struct s_room	*next;
 	struct s_room	*prev;
-	struct s_pos	pos;
 	char			*name;
-	int				nlinks;
-	char			full;
+	struct s_pos	pos;
+	long			ant;
+	int				weight;
 }					t_room;
+
+typedef struct		s_path
+{
+	struct s_room	*room;
+	int				length;
+}					t_path;
 
 typedef struct		s_env
 {
 	struct s_room	*start;
 	struct s_room	*end;
-	struct s_ant	*ants;
-	int				atstart;
+	struct s_path	**paths;
+	long			total_ants;
+	long			atstart;
 }					t_env;
-
-enum { ROOM, TUBE, COMM, START, END };
 
 #endif
