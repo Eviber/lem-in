1c6e72876cc304b3586b81a3f501434d07a19864
Youva GAUDE <ygaude@student.42.fr>
2017-12-13 19:56:34 +0100

    mod de lem-in.h

--- include/lem-in.h
+++ include/lem-in.h
@@ -6,33 +6,43 @@
 /*   By: ygaude <ygaude@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/03 15:28:55 by ygaude            #+#    #+#             */
-/*   Updated: 2017/12/03 18:58:36 by ygaude           ###   ########.fr       */
+/*   Updated: 2017/12/13 19:55:24 by ygaude           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEM_IN_H
 # define LEM_IN_H
 
+typedef struct		s_pos
+{
+	int				x;
+	int				y;
+}					t_pos;
+
 typedef struct		s_room
 {
-	struct s_room	*links;
+	struct s_room	**links;
+	struct s_room	*next;
+	struct s_room	*prev;
 	char			*name;
-	int				nlinks;
-	char			full;
+	struct s_pos	pos;
+	long			ant;
+	int				weight;
 }					t_room;
 
-typedef struct		s_ant
+typedef struct		s_path
 {
 	struct s_room	*room;
-	int				id;
-}					t_ant;
+	int				length;
+}					t_path;
 
 typedef struct		s_env
 {
 	struct s_room	*start;
 	struct s_room	*end;
-	struct s_ant	*ants;
-	int				atstart;
+	struct s_path	**paths;
+	long			total_ants;
+	long			atstart;
 }					t_env;
 
 #endif
