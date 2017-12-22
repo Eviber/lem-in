4051d22ce4e0a7903376ae8643b7ccdee0fc4d09
Pauline BOURMEAU <pbourmea@e3r2p14.42.fr>
2017-12-22 19:58:33 +0100

    mise a la norme parser need split 9 func

--- src/parser.c
+++ src/parser.c
@@ -1,7 +1,19 @@
-# include "cookielem-in.h"
-# include "get_next_line.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbourmea <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/22 19:51:52 by pbourmea          #+#    #+#             */
+/*   Updated: 2017/12/22 19:53:11 by pbourmea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void ft_error(unsigned long motif)
+#include "cookielem-in.h"
+#include "get_next_line.h"
+
+void		ft_error(unsigned long motif)
 {
 	if (motif == 1)
 		ft_putendl(ERR_ALLOC);
@@ -12,10 +24,13 @@ void ft_error(unsigned long motif)
 	exit(-1);
 }
 
-static void connect_rooms(t_map *map){
-	t_room *ptr1 = map->tmp[0];
-	t_room *ptr2 = map->tmp[1];
+static void	connect_rooms(t_map *map)
+{
+	t_room *ptr1;
+	t_room *ptr2;
 
+	ptr1 = map->tmp[0];
+	ptr2 = map->tmp[1];
 	ptr1->tubes = ptr1->a_tube;
 	ptr2->tubes = ptr2->a_tube;
 	while (ptr1->tubes->room)
@@ -28,12 +43,12 @@ static void connect_rooms(t_map *map){
 	ptr2->tubes->next = (t_tube*)ft_memalloc(sizeof(t_tube));
 }
 
-static int check_tubes(t_map *map, char *line)
+static int	check_tubes(t_map *map, char *line)
 {
-	int 	i;
-	t_room 	*ptr;
-	char 	**tube_data;
-	char 	status;
+	int		i;
+	t_room	*ptr;
+	char	**tube_data;
+	char	status;
 
 	i = 0;
 	status = 0;
@@ -41,7 +56,7 @@ static int check_tubes(t_map *map, char *line)
 	tube_data = ft_strsplit(line, '-');
 	while (tube_data[i] != NULL)
 		i++;
-	if (i!= 2)
+	if (i != 2)
 		ft_error(3);
 	ft_memset(map->tmp, 0, sizeof(t_room*) * 2);
 	while (ptr && status != 2)
@@ -56,7 +71,6 @@ static int check_tubes(t_map *map, char *line)
 		connect_rooms(map);
 	return (status);
 }
-
 
 static void	push_room(t_map *map, t_room *room)
 {
@@ -73,10 +87,11 @@ static void	push_room(t_map *map, t_room *room)
 	}
 }
 
-static void	room_print(t_room *room){
+static void	room_print(t_room *room)
+{
+	t_room *ptr;
 
-	t_room *ptr = room;
-
+	ptr = room;
 	while (ptr)
 	{
 		ft_putendl(ptr->name);
@@ -90,14 +105,14 @@ static void	room_print(t_room *room){
 	}
 }
 
-static void init_room(t_map *map, t_room *room, char **data, int p_status)
+static void	init_room(t_map *map, t_room *room, char **data, int p_status)
 {
 	room->name = ft_strdup(data[0]);
 	room->x = ft_atoi(data[1]);
 	room->y = ft_atoi(data[2]);
 	room->next = NULL;
 	room->tubes = ft_memalloc(sizeof(t_tube));
-	room->a_tube = 	room->tubes;
+	room->a_tube = room->tubes;
 	if (p_status == START)
 		map->start = room;
 	else if (p_status == END)
@@ -105,14 +120,12 @@ static void init_room(t_map *map, t_room *room, char **data, int p_status)
 	push_room(map, room);
 }
 
-
-
-static int linetodata(t_map *map, char *line, int p_status)
+static int	linetodata(t_map *map, char *line, int p_status)
 {
-	char 	**data;
-	t_room 	*room;
-	int i;
-	char 	status;
+	char	**data;
+	t_room	*room;
+	int		i;
+	char	status;
 
 	i = 0;
 	status = ROOM;
@@ -120,7 +133,7 @@ static int linetodata(t_map *map, char *line, int p_status)
 		ft_error(1);
 	if (ft_strchr(line, '-'))
 		status = TUBE;
-	if ((data = ft_strsplit(line, ' ')) == NULL )
+	if ((data = ft_strsplit(line, ' ')) == NULL)
 		ft_error(2);
 	while (data[i] != NULL)
 		i++;
@@ -132,7 +145,7 @@ static int linetodata(t_map *map, char *line, int p_status)
 	return (1);
 }
 
-static int ifisdigit(char *line)\
+static int	ifisdigit(char *line)\
 {
 	int i;
 
@@ -140,34 +153,33 @@ static int ifisdigit(char *line)\
 	while (line && line[i] != '\0')
 	{
 		if (!ft_isdigit(line[i]))
-			return(0);
+			return (0);
 		else
 			i++;
 	}
 	return (1);
 }
 
-
-int parser(t_map *map)
+int			parser(t_map *map)
 {
-	char 	*line;
-	int		status; /*0:no ant, 1:ant done, 2:start or 3:end */
+	char	*line;
+	int		status;
 	int		test;
 
 	status = NO_ANT;
 	line = NULL;
-	while((get_next_line(0, &line)) > 0)
+	while ((get_next_line(0, &line)) > 0)
 	{
 		if (*line && ifisdigit(line) && !status && (status = ANT))
 			map->ant = ft_atoi(line);
 		if (line[0] == '#' && line[1] == '#')
 		{
-			status = ((test = !(ft_strcmp(line + 2, "start")) +
-						(!ft_strcmp(line + 2, "end") * 2)) ? ANT + test : status);
+			test = !(ft_strcmp(line + 2, "start")) +
+			(!ft_strcmp(line + 2, "end") * 2);
+			status = ((test) ? ANT + test : status);
 		}
-		else
-			if ((status = ANT) && (linetodata(map, line, status)) == -1)
-				break;
+		else if ((status = ANT) && (linetodata(map, line, status)) == -1)
+			break ;
 		ft_putendl(line);
 		free(line);
 	}
